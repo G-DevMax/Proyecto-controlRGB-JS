4e3e086e9582eda247381ad41fafa8609cdb12e2
G-DevMax <martinezgustavo1012@gmail.com>
2024-09-28 18:53:50 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// Selecciona los elementos del DOM.
+const inputRojo = document.getElementById('rojo');
+const inputVerde = document.getElementById('verde');
+const inputAzul = document.getElementById('azul');
+
+const textoRojo = document.getElementById('texto-rojo');
+const textoVerde = document.getElementById('texto-verde');
+const textoAzul = document.getElementById('texto-azul');
+
+// Valores iniciales para definir el color RGB.
+let rojo = inputRojo.value;
+let verde = inputVerde.value;
+let azul = inputAzul.value;
+
+// Muestra los valores iniciales en los elementos <p>.
+textoRojo.textContent = rojo;
+textoVerde.textContent = verde;
+textoAzul.textContent = azul;
+
+// Función para actualizar el color de fondo de la página.
+function actualizarColor(rojo, verde, azul) {
+  document.body.style.backgroundColor = `rgb(${rojo}, ${verde}, ${azul})`;
+  /* Nota: 
+    En este caso para poder insertar las variables que contienen el numero, use "Template Literals" que basicamente es una forma de poder concatenar textos y variables,
+    para esto primero se debe de utilizar ``(comillas invertidas) y ya dentro se colocan las variables dentro de ${}. asi se separa lo que es texto, de lo que es una
+    variable y es más facil que hacer esto "rgb(" + rojo + ", " + verde + ", " + azul + ")";
+  */
+}
+
+// Agrega un event listener para el input de Rojo.
+inputRojo.addEventListener('change', (e) => {
+  rojo = e.target.value;
+  textoRojo.textContent = rojo;
+  actualizarColor(rojo, verde, azul);
+});
+
+// Agrega un event listener para el input de Verde.
+inputVerde.addEventListener('change', (e) => {
+  verde = e.target.value;
+  textoVerde.textContent = verde;
+  actualizarColor(rojo, verde, azul);
+});
+
+// Agrega un event listener para el input de Azul.
+inputAzul.addEventListener('change', (e) => {
+  azul = e.target.value;
+  textoAzul.textContent = azul;
+  actualizarColor(rojo, verde, azul);
+});
